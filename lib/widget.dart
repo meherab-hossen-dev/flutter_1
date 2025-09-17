17a0e309da9ee53825511ae86770928a88875d4f
unknown <mhnnishat0102@gmail.com>
2025-09-17 12:04:02 +0600

    notes & widget update

--- lib/widget.dart
+++ lib/widget.dart
@@ -5,6 +5,9 @@ class flutterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    TextEditingController userName = TextEditingController();
+    TextEditingController pass = TextEditingController();
     return Scaffold(
 
       appBar: AppBar(
@@ -23,27 +26,102 @@ class flutterWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(15),
             child: TextField(
-            
+              controller: userName,
               decoration: InputDecoration(
             
-                hintText: 'Enter your number',
+                hintText: 'Enter username',
                 hintStyle: TextStyle(
 
                   color: Colors.red,
                   fontSize: 15,
                 ),
-                labelText: 'Phone number',
+                labelText: 'User name',
                 labelStyle: TextStyle(
 
                   color: Colors.black,
                   fontSize: 20,
+                ),
+
+                border: OutlineInputBorder(
+
+                  borderRadius: BorderRadius.circular(50),
                 ),
                 prefixIcon: Icon(Icons.phone, color: Color.fromRGBO(46, 166, 228, 0.9019607843137255),),
                 // suffixIcon: Icon(Icons.check, color: Color.fromRGBO(46, 166, 228, 0.9019607843137255),),
 
               ),
             ),
-          )
+          ),
+
+          SizedBox(
+
+            height: 1,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: TextField(
+
+              obscureText: true,
+              controller: pass,
+              decoration: InputDecoration(
+
+                hintText: 'Enter your password',
+                hintStyle: TextStyle(
+
+                  color: Colors.red,
+                  fontSize: 15,
+                ),
+                labelText: 'Password',
+                labelStyle: TextStyle(
+
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+
+                border: OutlineInputBorder(
+
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                prefixIcon: Icon(Icons.password_sharp, color: Color.fromRGBO(46, 166, 228, 0.9019607843137255),),
+                 suffixIcon: Icon(Icons.remove_red_eye, color: Color.fromRGBO(46, 166, 228, 0.9019607843137255),),
+
+              ),
+            ),
+          ),
+
+          SizedBox(
+              width: 100,
+              height: 40,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+
+                    backgroundColor: Color.fromRGBO(46, 166, 228, 0.9019607843137255),
+                  ),
+                  onPressed: () {
+
+                    if(userName.text.isEmpty){
+
+                      print('Please enter username');
+                    }
+                    else{
+
+                      print('User name: ${userName.text}');
+                    }
+
+                    if(pass.text.contains('#')){
+
+                      print('Password: ${pass.text}');
+                    }
+                    else{
+
+                      print('Enter a password using \'#\'');
+                    }
+                  }, child: Text('Submit', style: TextStyle(
+
+                color: Colors.white,
+                fontSize: 16,
+              ),))),
         ],
       ),
     );
